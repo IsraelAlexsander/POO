e91fe3f01fc3a998160e33cef5e900f5f216d9f0
Israel Alexsander <rael3014@gmail.com>
2023-04-17 20:25:31 -0300

    Aula 17-04

---
+++ POO/IntroducaoForm/IntroducaoForm/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace IntroducaoForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            resources.ApplyResources(this.lblA, "lblA");
+            this.lblA.Name = "lblA";
+            // 
+            // txtA
+            // 
+            resources.ApplyResources(this.txtA, "txtA");
+            this.txtA.Name = "txtA";
+            this.txtA.TextChanged += new System.EventHandler(this.txtA_TextChanged);
+            this.txtA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtA_KeyPress);
+            // 
+            // btnSomar
+            // 
+            resources.ApplyResources(this.btnSomar, "btnSomar");
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            resources.ApplyResources(this.btnMultiplicar, "btnMultiplicar");
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnSubtrair
+            // 
+            resources.ApplyResources(this.btnSubtrair, "btnSubtrair");
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnDividir
+            // 
+            resources.ApplyResources(this.btnDividir, "btnDividir");
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // txtB
+            // 
+            resources.ApplyResources(this.txtB, "txtB");
+            this.txtB.Name = "txtB";
+            this.txtB.TextChanged += new System.EventHandler(this.txtB_TextChanged);
+            // 
+            // lblB
+            // 
+            resources.ApplyResources(this.lblB, "lblB");
+            this.lblB.Name = "lblB";
+            // 
+            // txtResult
+            // 
+            resources.ApplyResources(this.txtResult, "txtResult");
+            this.txtResult.Name = "txtResult";
+            // 
+            // lblResult
+            // 
+            resources.ApplyResources(this.lblResult, "lblResult");
+            this.lblResult.Name = "lblResult";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ POO/IntroducaoForm/IntroducaoForm/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroducaoForm
+{
+    public delegate double Operacao(double a, double b);
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }       
+
+        public double Soma(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtracao(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiplicacao(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divisao(double a, double b)
+        {
+            return a / b;
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            double Result;
+
+            Operacao Somar = new Operacao(Soma);
+
+            x = double.Parse(txtA.Text);
+            y = double.Parse(txtB.Text);
+
+            Result = Somar(x, y);
+
+            txtResult.Text = Result.ToString("N2");
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            double Result;
+
+            Operacao Subtrair = new Operacao(Subtracao);
+
+            x = double.Parse(txtA.Text);
+            y = double.Parse(txtB.Text);
+
+            Result = Subtrair(x, y);
+
+            txtResult.Text = Result.ToString("N2");
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            double Result;
+
+            Operacao Multiplicar = new Operacao(Multiplicacao);
+
+            x = double.Parse(txtA.Text);
+            y = double.Parse(txtB.Text);
+
+            Result = Multiplicar(x, y);
+
+            txtResult.Text = Result.ToString("N2");
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            double Result;
+
+            Operacao Dividir = new Operacao(Divisao);
+
+            x = double.Parse(txtA.Text);
+            y = double.Parse(txtB.Text);
+
+            Result = Dividir(x, y);
+
+            txtResult.Text = Result.ToString("N2");
+        }
+
+        private void txtA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+                txtB.Focus();
+        }
+
+        private void txtA_TextChanged(object sender, EventArgs e)
+        {
+            if (txtA.Text != "" && txtB.Text != "")
+            {
+                btnSomar.Enabled = true;
+                btnDividir.Enabled = true;
+                btnSubtrair.Enabled = true;
+                btnMultiplicar.Enabled = true;
+            }
+            else
+            {
+                btnSomar.Enabled = false;
+                btnDividir.Enabled = false;
+                btnSubtrair.Enabled = false;
+                btnMultiplicar.Enabled = false;
+            }
+        }
+
+        private void txtB_TextChanged(object sender, EventArgs e)
+        {
+            if (txtA.Text != "" && txtB.Text != "")
+            {
+                btnSomar.Enabled = true;
+                btnDividir.Enabled = true;
+                btnSubtrair.Enabled = true;
+                btnMultiplicar.Enabled = true;
+            }
+            else
+            {
+                btnSomar.Enabled = false;
+                btnDividir.Enabled = false;
+                btnSubtrair.Enabled = false;
+                btnMultiplicar.Enabled = false;
+            }
+        }
+    }
+}
