b5f39e5da51d03191117b5da3a209735c0308d5d
Israel Alexsander <rael3014@gmail.com>
2023-05-04 20:24:00 -0300

    Aula 04-05
    Aula sobre XML

---
+++ POO/32/32/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+
+namespace _32
+{
+    class Aluno
+    {
+        public string Matricula { get; set; }
+        public string Nome { get; set; }
+        public string eMail { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno xAluno;
+            List<Aluno> ListaAlunos = new List<Aluno>();
+
+            for(int i = 1; i <= 5; i++)
+            {
+                xAluno = new Aluno();
+
+                Console.Write("\nMatricula do Aluno: ");
+                xAluno.Matricula = Console.ReadLine();
+
+                Console.Write("Nome: ");
+                xAluno.Nome = Console.ReadLine();
+
+                Console.Write("E-Mail: ");
+                xAluno.eMail = Console.ReadLine();
+
+                ListaAlunos.Add(xAluno);
+                
+            }
+
+            string NomeArquivo = @"C:\Teste";    //@ serve para desconsiderar caracteres de controle
+            XElement AlunoXML = new XElement("Alunos", new XAttribute("Tipo","Regular"));
+
+            foreach(Aluno x in ListaAlunos)
+            {
+                AlunoXML.Add(new XElement("Aluno", new XElement("Matricula", x.Matricula), new XElement("Nome", x.Nome), new XElement("e-mail", x.eMail)));
+            }
+            AlunoXML.Save(NomeArquivo);
+
+            Console.WriteLine("Documento XML criado com sucesso!\n\n");
+            Console.ReadKey();
+
+            ListaAlunos.Clear();
+
+            AlunoXML = XElement.Load(NomeArquivo);
+            
+            Console.WriteLine($"Documento XML carregado com sucesso...");
+            Console.ReadKey();
+
+            xAluno = new Aluno();
+
+            var Query = from a in AlunoXML.Elements("Aluno") select a;
+
+            foreach(var Registro in Query)
+            {
+                xAluno.Matricula = Registro.Element("Matricula").Value;
+                xAluno.Nome = Registro.Element("Nome").Value;
+                xAluno.eMail = Registro.Element("e-mail").Value;
+
+                ListaAlunos.Add(xAluno);
+            }
+
+            Console.WriteLine($"Dados carregados: {ListaAlunos.Count} Registros\n\n");
+            Console.ReadKey();            
+        }
+    }
+}
