87910e0d812c4865ab5501ece4dd8cf532b1a9f0
Israel Alexsander <rael3014@gmail.com>
2023-05-22 19:53:09 -0300

    Aula 22-05
    Aula sobre formulário em WindowsForms

---
+++ POO/WindowsForms_Formulario/WindowsForms_Formulario/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Formulario
+{
+    public partial class frmIncremento : Form
+    {
+        FrmContagem FormContagem;
+
+        public frmIncremento(FrmContagem F)
+        {
+            InitializeComponent();
+            FormContagem = F;
+
+            lblContagem.Text = FormContagem.txtContagem.Text;
+        }
+
+        private void btnFechaForm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMais_Click(object sender, EventArgs e)
+        {
+            int Valor = int.Parse(FormContagem.txtContagem.Text);
+            Valor++;
+
+            FormContagem.txtContagem.Text = Valor.ToString();
+
+            lblContagem.Text = Valor.ToString();
+        }
+
+        private void btnMenos_Click(object sender, EventArgs e)
+        {
+            int Valor = int.Parse(FormContagem.txtContagem.Text);
+            Valor--;
+
+            FormContagem.txtContagem.Text = Valor.ToString();
+
+            lblContagem.Text = Valor.ToString();
+        }
+    }
+}
